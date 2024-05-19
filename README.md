0f62048a88de6752b3542f1fe4001b064320aa58
Zijian Zhang <doomspec@outlook.com>
2024-05-20 01:05:54 +0800

    update verify convex

---
+++ README.md
@@ -0,0 +1,10 @@
+# Install
+```bash
+git clone https://github.com/aspuru-guzik-group/CompositeMS.git
+cd　CompositeMS
+pip install -r requirements.txt
+```
+
+# Benchmark
+
+See the `benchmark` directory
--- benchmark/verify_convex.py
+++ benchmark/verify_convex.py
@@ -1,12 +1,13 @@
+from tqdm import trange
+
+from utils import get_ogm_n_heads, project_root
 from composite_ms.average_var import average_var_coeff_by_list_of_pwords
 from composite_ms.hamil import get_test_hamil
 from composite_ms.qubit_operator import QubitOperator
-from composite_ms.utils import project_root
 import numpy as np
 
 def f(coeff1, coeff2, pauli_tensor, hamil):
-    coeff1 = coeff1 / sum(coeff1)
-    coeff2 = coeff2 / sum(coeff2)
+
     f1 = average_var_coeff_by_list_of_pwords(
                 hamil, QubitOperator.from_pauli_tensor(coeff1, pauli_tensor))
     f2 = average_var_coeff_by_list_of_pwords(
@@ -17,16 +18,36 @@ def f(coeff1, coeff2, pauli_tensor, hamil):
     return ratio * f1 + (1-ratio) * f2, f_mid
 
 
-def main():
+def main_normalized():
     hamil_name = "LiH_12_JW"
+    get_ogm_n_heads(hamil_name)
     pword_dist = QubitOperator.read_op_file(
             hamil_name, project_root + "/scheme_saved/OG_not_optimized")
 
     coeff, pauli_tensor = pword_dist.get_pauli_tensor()
     hamil = get_test_hamil("mol", hamil_name)
-    res = f(np.random.random((len(coeff),)),np.random.random((len(coeff),)),pauli_tensor,hamil)
-    print(res)
+    coeff1 = np.random.random((len(coeff),))
+    coeff2 = np.random.random((len(coeff),))
+    coeff1 = coeff1 / sum(coeff1)
+    coeff2 = coeff2 / sum(coeff2)
+    res = f(coeff1, coeff2, pauli_tensor,hamil)
     assert res[0] > res[1]
 
-for i in range(10000):
-    main()
+def main_not_normalized():
+    hamil_name = "LiH_12_JW"
+    get_ogm_n_heads(hamil_name)
+    pword_dist = QubitOperator.read_op_file(
+            hamil_name, project_root + "/scheme_saved/OG_not_optimized")
+
+    coeff, pauli_tensor = pword_dist.get_pauli_tensor()
+    hamil = get_test_hamil("mol", hamil_name)
+    coeff1 = np.random.random((len(coeff),))
+    coeff2 = np.random.random((len(coeff),))
+    res = f(coeff1, coeff2, pauli_tensor,hamil)
+    assert res[0] > res[1]
+
+if __name__ == '__main__':
+    for i in trange(100):
+        main_normalized()
+    for i in trange(100):
+        main_not_normalized()
